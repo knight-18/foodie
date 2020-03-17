1aed91be84b2e8dce9dbfc9d7561e365a28059d1
KaviiSuri <surikavii@gmail.com>
2020-03-17 16:16:49 +0530

    added files and empty schema in models

---
+++ models/restaurant.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+var mongooseTypePhone = require("mongoose-type-phone");
+
+const Schema = mongoose.Schema;
+
+const RestaurantSchema = new Schema({});
+
+// Uncomment when done with schema
+// module.exports = Restaurant = mongoose.model("Restaurant", RestaurantSchema);
---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+var mongooseTypePhone = require("mongoose-type-phone");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({});
+// Uncomment when done with schema
+// module.exports = User = mongoose.model("User", UserSchema);
