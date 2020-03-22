66156e0ee02e882c889a2c2a0a94285c9686ba3a
knight-18 <sameershrivastava46@gmail.com>
2020-03-22 17:09:28 +0530

    Updated User Routes and user authorization

--- models/user.js
+++ models/user.js
@@ -1,8 +1,109 @@
 const mongoose = require("mongoose");
 var mongooseTypePhone = require("mongoose-type-phone");
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({});
-// Uncomment when done with schema
-// module.exports = User = mongoose.model("User", UserSchema);
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    orders: {
+        type: Array
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]}, 
+    {
+    timestamps: true
+    
+});
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
+
+//JWT function to generate auth tokens
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'foodie')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+//function to find user by email and verify password
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+//To hash the password before saving
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+module.exports = User = mongoose.model("User", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const User = require('../models/user')
+const auth = require('../middleware/userauth')
 const router = express.Router();
 
 //=========================== Routes==================================
@@ -28,4 +30,88 @@ router.get("/test", (req, res) => {
   res.send("[SUCCESS]: User routes connected!");
 });
 
-module.exports = router;
+//Route to create User
+router.post('/', async (req, res) => {
+  const user = new User(req.body)
+
+  try {
+      await user.save()
+      const token = await user.generateAuthToken()
+      res.status(201).send({ user, token })
+  } catch (e) {
+      res.status(400).send(e)
+  }
+})
+
+//Login Route for user
+router.post('/login', async (req, res) => {
+  try {
+      const user = await User.findByCredentials(req.body.email, req.body.password)
+      const token = await user.generateAuthToken()
+      res.send({ user, token })
+  } catch (e) {
+      res.status(400).send()
+  }
+})
+
+//Logout route for user
+router.post('/logout', auth, async (req, res) => {
+  try {
+      req.user.tokens = req.user.tokens.filter((token) => {
+          return token.token !== req.token
+      })
+      await req.user.save()
+
+      res.send()
+  } catch (e) {
+      res.status(500).send()
+  }
+})
+
+//Route to logout all sessions
+router.post('/logoutAll', auth, async (req, res) => {
+  try {
+      req.user.tokens = []
+      await req.user.save()
+      res.send()
+  } catch (e) {
+      res.status(500).send()
+  }
+})
+
+//Route to read user profile
+router.get('/me', auth, async (req, res) => {
+  res.send(req.user)
+})
+
+
+//Route for updating user profile
+router.patch('/me', auth, async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password','address','phone']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+      updates.forEach((update) => req.user[update] = req.body[update])
+      await req.user.save()
+      res.send(req.user)
+  } catch (e) {
+      res.status(400).send(e)
+  }
+})
+
+//Route to delete user profile
+router.delete('/me', auth, async (req, res) => {
+  try {
+      await req.user.remove()
+      res.send(req.user)
+  } catch (e) {
+      res.status(500).send()
+  }
+})
+
+module.exports = router
