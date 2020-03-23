462e3651a047f38e5fbc5ef3b10b764e0389f002
pathikrit2407 <52066229+pathikrit2407@users.noreply.github.com>
2020-03-23 13:06:52 +0530

    Update food.js

--- models/food.js
+++ models/food.js
@@ -2,7 +2,35 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const FoodSchema = new Schema({});
+const FoodSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    }
+});
+
+
+Food = mongoose.model("Food", FoodSchema);
+
+//==================for testing ===========================
+
+// var data =[
+//     {name:'chicken'},
+//     {name:'paneer'},
+//     {name:'cheese cake'}
+// ]
+// Food.create(data,(err,food)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("food created");
+//     }
+
+// });
+
+//======================================================
+
+
 
 // Uncomment when done with schema
-// module.exports = Food = mongoose.model("Food", FoodSchema);
+ module.exports = Food ;
