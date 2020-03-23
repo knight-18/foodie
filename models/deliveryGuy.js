b6064666ca407a8c6c3c4d1c4dc1e12a9baa3920
pathikrit2407 <52066229+pathikrit2407@users.noreply.github.com>
2020-03-23 13:02:17 +0530

    Update deliveryGuy.js

--- models/deliveryGuy.js
+++ models/deliveryGuy.js
@@ -3,7 +3,25 @@ var mongooseTypePhone = require("mongoose-type-phone");
 
 const Schema = mongoose.Schema;
 
-const DeliveryGuySchema = new Schema({});
+const DeliveryGuySchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type: String,
+        required:true
+    }
+});
+DeliveryGuy = mongoose.model("DeliveryGuy", DeliveryGuySchema);
 
+
+// DeliveryGuy.create({name:"abc",phone:123452221},(err,guy)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else 
+//     console.log(guy);
+// });
 // Uncomment when done with schema
-// module.exports = DeliveryGuy = mongoose.model("DeliveryGuy", DeliveryGuySchema);
+ module.exports = DeliveryGuy 
