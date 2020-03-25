4b1c504515e006e1d00e50ce5ae3672eacdd3c40
KaviiSuri <surikavii@gmail.com>
2020-03-26 01:26:37 +0530

    added ref's to the user, restaurant and deliveryGuy shemas referencing the Order in order to create links between them

--- models/deliveryGuy.js
+++ models/deliveryGuy.js
@@ -1,105 +1,112 @@
 const mongoose = require("mongoose");
 var mongooseTypePhone = require("mongoose-type-phone");
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const Schema = mongoose.Schema;
 
-const DeliveryGuySchema = new Schema({
-    name:{
-        type:String,
-        required:true
+const DeliveryGuySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
     },
-    phone:{
-        type: String,
-        required:true
+    phone: {
+      type: String,
+      required: true
     },
-    username:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
     },
     password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"')
-            }
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error('Password cannot contain "password"');
         }
+      }
     },
-    orders: {
-        type: Array
-    },
-    tokens: [{
+    orders: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Order"
+      }
+    ],
+    tokens: [
+      {
         token: {
-            type: String,
-            required: true
+          type: String,
+          required: true
         }
-    }]}, 
-    {
+      }
+    ]
+  },
+  {
     timestamps: true
-});
+  }
+);
 
-DeliveryGuySchema.methods.toJSON = function () {
-    const user = this
-    const userObject = user.toObject()
+DeliveryGuySchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
 
-    delete userObject.password
-    delete userObject.tokens
+  delete userObject.password;
+  delete userObject.tokens;
 
-    return userObject
-}
-
+  return userObject;
+};
 
 //JWT function to generate auth tokens
-DeliveryGuySchema.methods.generateAuthToken = async function () {
-    const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
+DeliveryGuySchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
 
-    user.tokens = user.tokens.concat({ token })
-    await user.save()
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
 
-    return token
-}
+  return token;
+};
 
 //function to find user by email and verify password
 DeliveryGuySchema.statics.findByCredentials = async (username, password) => {
-    const user = await DeliveryGuy.findOne({ username })
+  const user = await DeliveryGuy.findOne({ username });
 
-    if (!user) {
-        throw new Error('Unable to login')
-    }
+  if (!user) {
+    throw new Error("Unable to login");
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password)
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        throw new Error('Unable to login')
-    }
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
 
-    return user
-}
+  return user;
+};
 
 //To hash the password before saving
-DeliveryGuySchema.pre('save', async function (next) {
-    const user = this
+DeliveryGuySchema.pre("save", async function(next) {
+  const user = this;
 
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
-    }
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
 
-    next()
-})
+  next();
+});
 
 // DeliveryGuy.create({name:"abc",phone:123452221},(err,guy)=>{
 //     if(err){
 //         console.log(err);
 //     }
-//     else 
+//     else
 //     console.log(guy);
 // });
 module.exports = DeliveryGuy = mongoose.model("DeliveryGuy", DeliveryGuySchema);
