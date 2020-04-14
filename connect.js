45ccee2fd48db27ff9082cb85e4a443750c59f5c
KaviiSuri <surikavii@gmail.com>
2020-04-14 20:33:10 +0530

    added the right mongodb url

--- connect.js
+++ connect.js
@@ -6,8 +6,10 @@ function connectDB() {
     case "dev":
       url = "mongodb://localhost:27017/foodie";
       break;
-    case "prod":
+    case "production":
       /* make url your production connection string for database */
+      url =
+        "mongodb+srv://test-user:2I4Llmp05T53EejP@cluster0-sndxq.mongodb.net/test?retryWrites=true&w=majority";
       break;
     default:
       // In dev mode by default
@@ -18,9 +20,9 @@ function connectDB() {
     {
       useNewUrlParser: true,
       useFindAndModify: false,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     },
-    err => {
+    (err) => {
       if (err) console.log(err);
       else console.log("Database Connected!");
     }
